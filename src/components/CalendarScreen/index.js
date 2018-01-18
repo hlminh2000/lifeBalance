55875a14c6e9963144a7a3cee72f6894349e44af
Minh <hlminh2000@gmail.com>
2018-01-18 01:04:59 -0500

    adds calendar

--- src/components/CalendarScreen/index.js
+++ src/components/CalendarScreen/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon } from 'native-base'
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 
 const CalendarScreen = () => (
   <Container>
@@ -16,17 +17,21 @@ const CalendarScreen = () => (
       <Right />
     </Header>
     <Content>
-      <Text>
-        This is Content Section
-      </Text>
+      <Calendar
+        current={'2012-03-01'}
+        minDate={'2012-05-10'}
+        maxDate={'2012-05-30'}
+        onDayPress={(day) => {console.log('selected day', day)}}
+        monthFormat={'yyyy MM'}
+        onMonthChange={(month) => {console.log('month changed', month)}}
+        hideArrows={true}
+        renderArrow={(direction) => (<Arrow />)}
+        hideExtraDays={true}
+        disableMonthChange={true}
+        firstDay={1}
+        hideDayNames={true}
+        showWeekNumbers={true}/>
     </Content>
-    <Footer>
-      <FooterTab>
-        <Button full>
-          <Text>Footer</Text>
-        </Button>
-      </FooterTab>
-    </Footer>
   </Container>
 )
 
