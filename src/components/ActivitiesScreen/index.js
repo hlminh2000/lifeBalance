cd7d6ca725eae52375c254e2d625ba0d60b6698a
Minh <hlminh2000@gmail.com>
2018-01-26 21:32:49 -0500

    indentation

--- src/components/ActivitiesScreen/index.js
+++ src/components/ActivitiesScreen/index.js
@@ -1,34 +1,43 @@
-import React, { Component } from 'react';
-import { Text, View, TouchableHighlight, ListView } from 'react-native'
+import React, { Component } from "react";
+import { Text, View, TouchableHighlight, ListView } from "react-native";
 import {
-  Container, Content, Button,
-  Left, Right, Body,
-  Card, CardItem, H1,
-  Label, Form
-} from 'native-base'
-import FAB from 'react-native-fab'
-import HeaderBar from '../HeaderBar/index.js'
-import { connect } from 'react-redux'
-import actions from './actions.js'
-import CheckBox from '../reusables/Checkbox.js'
-import Input from '../reusables/Input.js'
-import Modal from "react-native-modal"
-import ActivityItemList from './ActivityItemList/index.js'
+  Container,
+  Content,
+  Button,
+  Left,
+  Right,
+  Body,
+  Card,
+  CardItem,
+  H1,
+  Label,
+  Form
+} from "native-base";
+import FAB from "react-native-fab";
+import HeaderBar from "../HeaderBar/index.js";
+import { connect } from "react-redux";
+import actions from "./actions.js";
+import CheckBox from "../reusables/Checkbox.js";
+import Input from "../reusables/Input.js";
+import Modal from "react-native-modal";
+import ActivityItemList from "./ActivityItemList/index.js";
 
 const ActivityEditModal = ({
   activity,
   onCancel,
   onComplete,
-  onTitleChange,
+  onTitleChange
 }) => (
   <Modal isVisible={activity !== null}>
-    <Card style={{
-      flex: 0,
-      marginLeft: 20,
-      marginRight: 20,
-      paddingTop: 10,
-      paddingBottom: 10,
-    }}>
+    <Card
+      style={{
+        flex: 0,
+        marginLeft: 20,
+        marginRight: 20,
+        paddingTop: 10,
+        paddingBottom: 10
+      }}
+    >
       <CardItem>
         <Left>
           <Body>
@@ -40,24 +49,24 @@ const ActivityEditModal = ({
         <Left>
           <Body>
             <Input
-              autoFocus={ true }
-              label={ "Title" }
-              value={ activity ? activity.title : "" }
-              onChangeText={ onTitleChange }
+              autoFocus={true}
+              label={"Title"}
+              value={activity ? activity.title : ""}
+              onChangeText={onTitleChange}
             />
           </Body>
         </Left>
       </CardItem>
       <CardItem>
-        <Left></Left>
-        <Body></Body>
+        <Left />
+        <Body />
         <Right>
-          <View style={{ flexDirection: 'row' }}>
-            <TouchableHighlight onPress={ onCancel }>
+          <View style={{ flexDirection: "row" }}>
+            <TouchableHighlight onPress={onCancel}>
               <Text>Cancel</Text>
             </TouchableHighlight>
             <Text style={{ width: 30 }}> </Text>
-            <TouchableHighlight onPress={ onComplete }>
+            <TouchableHighlight onPress={onComplete}>
               <Text>Done</Text>
             </TouchableHighlight>
           </View>
@@ -65,7 +74,7 @@ const ActivityEditModal = ({
       </CardItem>
     </Card>
   </Modal>
-)
+);
 
 const ActivitiesScreen = ({
   activities,
@@ -76,45 +85,45 @@ const ActivitiesScreen = ({
   onNewActivityComplete,
   onNewActivityCancel,
   navigation,
-  onActivityDelete,
- }) => {
-  return (
-    <Container>
-      <HeaderBar navigation={navigation} title="My Activities"/>
-      <Content>
-        <ActivityItemList
-          onInfoPressed={ activity => console.log(activity) }
-          onDeletePressed={ activity => onActivityDelete(activity.id) }
-          activities={ activities }
-        />
-      </Content>
-      <FAB buttonColor="red" onClickAction={ onFabTapped } />
-      <ActivityEditModal
-        activity={ newStagingActivity }
-        onCancel={ onNewActivityCancel }
-        onComplete={ onNewActivityComplete }
-        onTitleChange={ onNewActivityTitleChange }
+  onActivityDelete
+}) => (
+  <Container>
+    <HeaderBar navigation={navigation} title="My Activities" />
+    <Content>
+      <ActivityItemList
+        onInfoPressed={activity => console.log(activity)}
+        onDeletePressed={activity => onActivityDelete(activity.id)}
+        activities={activities}
       />
-    </Container>
-  )
-}
+    </Content>
+    <FAB buttonColor="red" onClickAction={onFabTapped} />
+    <ActivityEditModal
+      activity={newStagingActivity}
+      onCancel={onNewActivityCancel}
+      onComplete={onNewActivityComplete}
+      onTitleChange={onNewActivityTitleChange}
+    />
+  </Container>
+);
 
 export default connect(
   state => ({
     activities: state.activitiesScreen.activityList,
     editingActivityId: state.activitiesScreen.currentEditingActivityId,
-    newStagingActivity: state.activitiesScreen.newStagingActivity,
+    newStagingActivity: state.activitiesScreen.newStagingActivity
   }),
   dispatch => ({
-    onFabTapped: (activity) => dispatch(
-      actions['ACTIVITIES_SCREEN/STAGE_NEW_ACTIVITY'].create()),
-    onNewActivityTitleChange: (newText) =>  dispatch(
-      actions['ACTIVITIES_SCREEN/NEW_ACTIVITY_TITLE_CHANGE'].create(newText)),
-    onNewActivityComplete: () => dispatch(
-      actions['ACTIVITIES_SCREEN/NEW_ACTIVITY_COMPLETE'].create()),
-    onNewActivityCancel: () => dispatch(
-      actions['ACTIVITIES_SCREEN/NEW_ACTIVITY_CANCEL'].create()),
-    onActivityDelete: (activityId) => dispatch(
-      actions['ACTIVITIES_SCREEN/ACTIVITY_DELETE'].create(activityId)),
+    onFabTapped: activity =>
+      dispatch(actions["ACTIVITIES_SCREEN/STAGE_NEW_ACTIVITY"].create()),
+    onNewActivityTitleChange: newText =>
+      dispatch(
+        actions["ACTIVITIES_SCREEN/NEW_ACTIVITY_TITLE_CHANGE"].create(newText)
+      ),
+    onNewActivityComplete: () =>
+      dispatch(actions["ACTIVITIES_SCREEN/NEW_ACTIVITY_COMPLETE"].create()),
+    onNewActivityCancel: () =>
+      dispatch(actions["ACTIVITIES_SCREEN/NEW_ACTIVITY_CANCEL"].create()),
+    onActivityDelete: activityId =>
+      dispatch(actions["ACTIVITIES_SCREEN/ACTIVITY_DELETE"].create(activityId))
   })
-)(ActivitiesScreen)
+)(ActivitiesScreen);
