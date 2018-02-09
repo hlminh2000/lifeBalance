f5afd7aae4ace6cf3d604113985d15579ae72260
Minh <hlminh2000@gmail.com>
2018-02-08 21:20:35 -0500

    some new utilities hookup

--- src/components/CalendarScreen/index.js
+++ src/components/CalendarScreen/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { StyleSheet, View, CheckBox } from "react-native";
+import State from "../State.js";
 import actions from "./actions";
 import {
   Container,
@@ -21,16 +22,21 @@ import {
 import { Calendar, CalendarList, Agenda } from "react-native-calendars";
 import HeaderBar from "../HeaderBar/index.js";
 import icons from "../icons";
+import DateUtils from "../../utils/DateUtils";
 
 const CalendarScreen = ({
   day,
   navigation,
   availableActivities,
-  onActivityCheck
+  onActivityCheck,
+  currentDayString = DateUtils.getCurrentDateString()
 }) => (
   <Container>
     <HeaderBar navigation={navigation} title="My Calendar" />
     <Calendar
+      markedDates={{
+        [currentDayString]: { selected: true }
+      }}
       style={{ elevation: 2 }}
       onDayPress={day => {
         console.log("selected day", day);
