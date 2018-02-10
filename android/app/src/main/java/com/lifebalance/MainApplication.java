bcc9a6bfc773905287f12615e9afc1463eb94c22
Minh <hlminh2000@gmail.com>
2018-02-09 22:21:35 -0500

    adds push notification for later

--- android/app/src/main/java/com/lifebalance/MainApplication.java
+++ android/app/src/main/java/com/lifebalance/MainApplication.java
@@ -3,6 +3,7 @@ package com.lifebalance;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.mikemonteith.reactnativeandroidcheckbox.CheckboxPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new CheckboxPackage(),
             new LinearGradientPackage(),
             new VectorIconsPackage()
