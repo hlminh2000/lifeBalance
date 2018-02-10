b695597c38f26261d0ce3c3736582a3dad5ef0f5
Minh <hlminh2000@gmail.com>
2018-02-09 23:45:17 -0500

    adds appcenter and sets up apk signature

--- android/app/src/main/java/com/lifebalance/MainApplication.java
+++ android/app/src/main/java/com/lifebalance/MainApplication.java
@@ -3,6 +3,9 @@ package com.lifebalance;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
+import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
+import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.mikemonteith.reactnativeandroidcheckbox.CheckboxPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -27,6 +30,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appcenterCrashes_whenToSendCrashes)),
+            new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appcenterAnalytics_whenToEnableAnalytics)),
+            new AppCenterReactNativePackage(MainApplication.this),
             new ReactNativePushNotificationPackage(),
             new CheckboxPackage(),
             new LinearGradientPackage(),
