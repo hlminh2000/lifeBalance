da59c6f07f8d3327115abc6a43c90917c457842c
Minh <hlminh2000@gmail.com>
2018-02-25 14:16:33 -0500

    proof of concept pixi component

--- src/components/CalendarScreen/CircularSlider/index.js
+++ src/components/CalendarScreen/CircularSlider/index.js
@@ -1,28 +1,17 @@
 import React from "react";
-import Canvas from "react-native-canvas";
 import { WebView } from "react-native";
 
-class CustomCanvas extends Canvas {
-  constructor(props) {
-    super(props);
-  }
-}
-
 export default class CircularSlider extends React.Component {
-  handleCanvas = canvas => {
-    console.log(canvas);
-  };
-
   render() {
     return (
-      <CustomCanvas
+      <WebView
+        source={require("./webComponent/index.html")}
         style={{
           width: 200,
           height: 200,
           borderColor: "#000000",
           borderWidth: 0.5
         }}
-        ref={this.handleCanvas}
       />
     );
   }
