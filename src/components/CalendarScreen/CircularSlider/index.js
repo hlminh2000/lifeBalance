a711bbebeb603ff2e99a2a084dfb36793e9e1f74
Minh <hlminh2000@gmail.com>
2018-03-01 20:55:18 -0500

    component is working

--- src/components/CalendarScreen/CircularSlider/index.js
+++ src/components/CalendarScreen/CircularSlider/index.js
@@ -1,21 +1,54 @@
 import React from "react";
-import { WebView } from "react-native";
+import { WebView, View } from "react-native";
 
 export default class CircularSlider extends React.Component {
   render() {
+    const {
+      radius = 250,
+      maxValue = 720,
+      minValue = 0,
+      arcThickness = 70,
+      arcColor = 0xd84132,
+      minValueInitial = 10,
+      maxValueInitial = 15,
+      interval = 2,
+      onValueChange = minMax => {}
+    } = this.props;
     return (
-      <WebView
-        source={require("./webComponent/index.html")}
-        style={{
-          position: "absolute",
-          top: 0,
-          bottom: 0,
-          left: 0,
-          right: 0,
-          borderColor: "#000000",
-          borderWidth: 0.5
-        }}
-      />
+      <View>
+        <WebView
+          source={require("./webComponent/index.html")}
+          onMessage={e => {
+            const { payload: { state: { minValue, maxValue } } } = JSON.parse(
+              e.nativeEvent.data
+            );
+            onValueChange({
+              min: minValue,
+              max: maxValue
+            });
+          }}
+          injectedJavaScript={`
+            window.radius = ${radius};
+            window.maxValue = ${maxValue};
+            window.minValue = ${minValue};
+            window.arcThickness = ${arcThickness};
+            window.arcColor = ${arcColor};
+            window.minValueInitial = ${minValueInitial};
+            window.maxValueInitial = ${maxValueInitial};
+            window.interval = ${maxValueInitial};
+            init();
+          `}
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: radius + arcThickness,
+            height: radius + arcThickness,
+            borderColor: "#000000",
+            borderWidth: 0.5
+          }}
+        />
+      </View>
     );
   }
 }
--- src/components/CalendarScreen/CircularSlider/webComponent/index.js
+++ src/components/CalendarScreen/CircularSlider/webComponent/index.js
@@ -1,159 +1,167 @@
-const containerDiv = document.getElementById("appContainer");
-const zoomLevel = 1;
-const canvasWidth = containerDiv.offsetWidth * 2;
-const canvasHeight = containerDiv.offsetHeight * 2;
-const screenCenter = {
-  x: canvasWidth / (2 * zoomLevel),
-  y: canvasHeight / (2 * zoomLevel)
-};
-const config = {
-  radius: 200,
-  maxValue: 20,
-  minValue: 0,
-  thickness: 60,
-  archColor: 0x42deae
-};
-const modelState = {
-  minValue: 0,
-  maxValue: 10,
-  updatingField: false
-};
-
-const dispatchPublicEvent = ({ message, payload }) => {
-  window.postMessage({ message, payload }, "*");
-};
-
-const pixiApp = new PIXI.Application({
-  view: document.getElementById("mainCanvas"),
-  width: canvasWidth,
-  height: canvasHeight,
-  antialias: true,
-  resolution: zoomLevel,
-  transparent: true
-});
-const stage = pixiApp.stage;
-stage.interactive = true;
-
-window.ctx = document.getElementById("mainCanvas").getContext("2d");
-
-const degreeToRadians = degrees => degrees * Math.PI / 180;
-const radiansToDegree = radians => radians * 180 / Math.PI;
-
-const valueToRadian = val => {
-  const center = screenCenter;
-  const { radius, maxValue, minValue } = config;
-  const fullRange = maxValue - minValue;
-  const degreeAngle = (val - minValue) / fullRange * 360 - 90;
-  const radiansAngle = degreeToRadians(degreeAngle);
-  return radiansAngle;
-};
-
-const valueToPosition = val => {
-  const { radius } = config;
-  const radiansAngle = valueToRadian(val);
-  const sin = Math.sin(radiansAngle);
-  const cos = Math.cos(radiansAngle);
-  const position = {
-    x: screenCenter.x + cos * radius,
-    y: screenCenter.y + sin * radius
+const init = () => {
+  const containerDiv = document.getElementById("appContainer");
+  const zoomLevel = 1;
+  const canvasWidth = containerDiv.offsetWidth * 2;
+  const canvasHeight = containerDiv.offsetHeight * 2;
+  const screenCenter = {
+    x: canvasWidth / (2 * zoomLevel),
+    y: canvasHeight / (2 * zoomLevel)
   };
-  return position;
-};
+  const config = {
+    radius: window.radius || 200,
+    maxValue: window.maxValue || 1440,
+    minValue: window.minValue || 0,
+    arcThickness: window.arcThickness || 60,
+    arcColor: window.arcColor || 0x42deae,
+    minValueInitial: window.minValueInitial || 0,
+    maxValueInitial: window.maxValueInitial || 5,
+    interval: window.interval || 100
+  };
+  const modelState = {
+    minValue: config.minValueInitial,
+    maxValue: config.maxValueInitial,
+    updatingField: false
+  };
 
-const positionToValue = ({ x, y }) => {
-  const deltaX = x - screenCenter.x;
-  const deltaY = y - screenCenter.y;
-  const diag = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
-  const radians = Math.asin(deltaY / diag);
-  const degreeAngle = radiansToDegree(radians) + 90;
-  const value =
-    degreeAngle / 360 * (config.maxValue - config.minValue) + config.minValue;
-  return deltaX >= 0 ? value : -value;
-};
+  const dispatchPublicEvent = ({ message, payload }) => {
+    window.postMessage(JSON.stringify({ message, payload }), "*");
+    console.log(message, payload);
+  };
 
-const createScrubber = ({ stateModelKey, color = 0xffffff, radius = 5 }) => {
-  const backDrop = new PIXI.Graphics()
-    .beginFill(config.archColor)
-    .drawCircle(0, 0, radius);
-  const sprite = new PIXI.Graphics()
-    .beginFill(color)
-    .drawCircle(0, 0, radius * 0.9);
-  const hitArea = new PIXI.Graphics()
-    .beginFill(color, 0)
-    .drawCircle(0, 0, radius * 5);
-  hitArea.interactive = true;
-  hitArea.on("pointerdown", e => (modelState.updatingField = stateModelKey));
-  stage.on("pointermove", e => {
-    if (modelState.updatingField === stateModelKey) {
-      modelState[stateModelKey] = positionToValue(e.data.global);
-      dispatchPublicEvent({
-        message: "VALUE_CHANGE",
-        payload: {
-          state: modelState
-        }
-      });
-    }
+  const pixiApp = new PIXI.Application({
+    view: document.getElementById("mainCanvas"),
+    width: canvasWidth,
+    height: canvasHeight,
+    antialias: true,
+    resolution: zoomLevel,
+    transparent: true
   });
-  stage.on("pointerup", e => (modelState.updatingField = null));
-  backDrop.addChild(sprite);
-  sprite.addChild(hitArea);
-  sprite.interactive = true;
-  sprite.buttonMode = true;
-  return backDrop;
-};
+  const stage = pixiApp.stage;
+  stage.interactive = true;
 
-const createArch = ({
-  color = 0,
-  thickness = config.thickness,
-  reverse = false,
-  opacity = 1
-}) => {
-  const archGraphics = new PIXI.Graphics();
-  archGraphics.render = () => {
-    archGraphics
-      .clear()
-      .lineStyle(thickness, color, opacity)
-      .arc(
-        0,
-        0,
-        config.radius,
-        reverse
-          ? valueToRadian(modelState.maxValue)
-          : valueToRadian(modelState.minValue),
-        reverse
-          ? valueToRadian(modelState.minValue)
-          : valueToRadian(modelState.maxValue)
-      );
+  window.ctx = document.getElementById("mainCanvas").getContext("2d");
+
+  const degreeToRadians = degrees => degrees * Math.PI / 180;
+  const radiansToDegree = radians => radians * 180 / Math.PI;
+
+  const valueToRadian = val => {
+    const center = screenCenter;
+    const { radius, maxValue, minValue } = config;
+    const fullRange = maxValue - minValue;
+    const degreeAngle = (val - minValue) / fullRange * 360 - 90;
+    const radiansAngle = degreeToRadians(degreeAngle);
+    return radiansAngle;
   };
-  archGraphics.x = screenCenter.x;
-  archGraphics.y = screenCenter.y;
-  return archGraphics;
+
+  const valueToPosition = val => {
+    const { radius } = config;
+    const radiansAngle = valueToRadian(val);
+    const sin = Math.sin(radiansAngle);
+    const cos = Math.cos(radiansAngle);
+    const position = {
+      x: screenCenter.x + cos * radius,
+      y: screenCenter.y + sin * radius
+    };
+    return position;
+  };
+
+  const positionToValue = ({ x, y }) => {
+    const { maxValue, interval } = config;
+    const deltaX = x - screenCenter.x;
+    const deltaY = y - screenCenter.y;
+    const diag = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+    const radians = Math.asin(deltaY / diag);
+    const degreeAngle = radiansToDegree(radians) + 90;
+    const value =
+      degreeAngle / 360 * (config.maxValue - config.minValue) + config.minValue;
+    const roundToInterval = val => Math.round(val / interval) * interval;
+    return roundToInterval(deltaX >= 0 ? value : maxValue - value);
+  };
+
+  const createScrubber = ({ stateModelKey, color = 0xffffff, radius = 5 }) => {
+    const backDrop = new PIXI.Graphics()
+      .beginFill(config.arcColor)
+      .drawCircle(0, 0, radius);
+    const sprite = new PIXI.Graphics()
+      .beginFill(color)
+      .drawCircle(0, 0, radius * 0.9);
+    const hitArea = new PIXI.Graphics()
+      .beginFill(color, 0)
+      .drawCircle(0, 0, radius * 5);
+    hitArea.interactive = true;
+    hitArea.on("pointerdown", e => (modelState.updatingField = stateModelKey));
+    stage.on("pointermove", e => {
+      if (modelState.updatingField === stateModelKey) {
+        modelState[stateModelKey] = positionToValue(e.data.global);
+        dispatchPublicEvent({
+          message: "VALUE_CHANGE",
+          payload: {
+            state: modelState
+          }
+        });
+      }
+    });
+    stage.on("pointerup", e => (modelState.updatingField = null));
+    backDrop.addChild(sprite);
+    sprite.addChild(hitArea);
+    sprite.interactive = true;
+    sprite.buttonMode = true;
+    return backDrop;
+  };
+
+  const createArch = ({
+    color = 0,
+    arcThickness = config.arcThickness,
+    reverse = false,
+    opacity = 1
+  }) => {
+    const archGraphics = new PIXI.Graphics();
+    archGraphics.render = () => {
+      archGraphics
+        .clear()
+        .lineStyle(arcThickness, color, opacity)
+        .arc(
+          0,
+          0,
+          config.radius,
+          reverse
+            ? valueToRadian(modelState.maxValue)
+            : valueToRadian(modelState.minValue),
+          reverse
+            ? valueToRadian(modelState.minValue)
+            : valueToRadian(modelState.maxValue)
+        );
+    };
+    archGraphics.x = screenCenter.x;
+    archGraphics.y = screenCenter.y;
+    return archGraphics;
+  };
+
+  const arch = createArch({ color: config.arcColor });
+  const reverseArc = createArch({ reverse: true, color: 0, opacity: 0.2 });
+  const minCircleSprite = createScrubber({
+    stateModelKey: "minValue",
+    color: 0xffffff,
+    radius: config.arcThickness / 2
+  });
+  const maxCircleSprite = createScrubber({
+    stateModelKey: "maxValue",
+    color: 0xffffff,
+    radius: config.arcThickness / 2
+  });
+
+  stage.addChild(arch);
+  stage.addChild(reverseArc);
+  stage.addChild(minCircleSprite);
+  stage.addChild(maxCircleSprite);
+
+  // main animation loop
+  pixiApp.ticker.add(() => {
+    minCircleSprite.x = valueToPosition(modelState.minValue).x;
+    minCircleSprite.y = valueToPosition(modelState.minValue).y;
+    maxCircleSprite.x = valueToPosition(modelState.maxValue).x;
+    maxCircleSprite.y = valueToPosition(modelState.maxValue).y;
+    arch.render();
+    reverseArc.render();
+  });
 };
-
-const arch = createArch({ color: config.archColor });
-const reverseArc = createArch({ reverse: true, color: 0, opacity: 0.2 });
-const minCircleSprite = createScrubber({
-  stateModelKey: "minValue",
-  color: 0xffffff,
-  radius: config.thickness / 2
-});
-const maxCircleSprite = createScrubber({
-  stateModelKey: "maxValue",
-  color: 0xffffff,
-  radius: config.thickness / 2
-});
-
-stage.addChild(arch);
-stage.addChild(reverseArc);
-stage.addChild(minCircleSprite);
-stage.addChild(maxCircleSprite);
-
-// main animation loop
-pixiApp.ticker.add(() => {
-  minCircleSprite.x = valueToPosition(modelState.minValue).x;
-  minCircleSprite.y = valueToPosition(modelState.minValue).y;
-  maxCircleSprite.x = valueToPosition(modelState.maxValue).x;
-  maxCircleSprite.y = valueToPosition(modelState.maxValue).y;
-  arch.render();
-  reverseArc.render();
-});
