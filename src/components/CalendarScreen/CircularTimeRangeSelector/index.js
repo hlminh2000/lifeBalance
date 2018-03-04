ce1090b2a9608f63d699158050b70604f3c9f034
Minh <hlminh2000@gmail.com>
2018-03-03 19:51:46 -0500

    renders from and to time

--- src/components/CalendarScreen/CircularTimeRangeSelector/index.js
+++ src/components/CalendarScreen/CircularTimeRangeSelector/index.js
@@ -21,57 +21,80 @@ export default class CircularTimeRangeSlider extends Component {
       minutes: diff - hours * 60
     };
   };
+  getTimeFromMinute = minutes => {
+    const hours = Math.floor(minutes / 60);
+    return {
+      hours,
+      minutes: minutes - hours * 60
+    };
+  };
   render() {
+    const { minValue: from, maxValue: to } = this.state;
     const radius = 250;
     const arcThickness = 70;
     const dimention = radius + arcThickness / 2;
     const deltaTime = this.getDeltaTime();
+    const timeFrom = this.getTimeFromMinute(from);
+    const timeTo = this.getTimeFromMinute(to);
+    const leftPad = num => (num > 9 ? num : `0${num}`);
     return (
-      <View
-        style={{
-          width: dimention,
-          height: dimention,
-          justifyContent: "center",
-          alignItems: "center"
-        }}
-      >
+      <View style={{ alignItems: "center" }}>
+        <View style={{ justifyContent: "space-around", flexDirection: "row" }}>
+          <View>
+            <Text>{`${leftPad(timeFrom.hours)}:${leftPad(
+              timeFrom.minutes
+            )}`}</Text>
+          </View>
+          <View>
+            <Text>{`${leftPad(timeTo.hours)}:${leftPad(timeTo.minutes)}`}</Text>
+          </View>
+        </View>
         <View
           style={{
             width: dimention,
             height: dimention,
-            position: "absolute",
-            top: 0,
-            left: 0
+            justifyContent: "center",
+            alignItems: "center"
           }}
         >
-          <CircularSlider
-            {...{
-              minValue: 0,
-              maxValue: 1440,
-              interval: 15,
-              radius,
-              arcThickness,
-              arcColor: eval(style.COLOR_PRIMARY.split("#").join("0x")),
-              minValueInitial: this.state.minValue,
-              maxValueInitial: this.state.maxValue,
-              onValueChange: ({ min, max }) => {
-                this.setState({
-                  minValue: min,
-                  maxValue: max
-                });
-              }
+          <View
+            style={{
+              width: dimention,
+              height: dimention,
+              position: "absolute",
+              top: 0,
+              left: 0
             }}
-          />
-        </View>
-        <View style={{ flexDirection: "row" }}>
-          <Text style={{ color: style.COLOR_PRIMARY, fontSize: 30 }}>
-            {`${deltaTime.hours > 9 ? deltaTime.hours : `0${deltaTime.hours}`}`}
-          </Text>
-          <Text>h</Text>
-          <Text style={{ color: style.COLOR_PRIMARY, fontSize: 30 }}>{` ${
-            deltaTime.minutes > 9 ? deltaTime.minutes : `0${deltaTime.minutes}`
-          }`}</Text>
-          <Text>m</Text>
+          >
+            <CircularSlider
+              {...{
+                minValue: 0,
+                maxValue: 1440,
+                interval: 15,
+                radius,
+                arcThickness,
+                arcColor: eval(style.COLOR_PRIMARY.split("#").join("0x")),
+                minValueInitial: this.state.minValue,
+                maxValueInitial: this.state.maxValue,
+                onValueChange: ({ min, max }) => {
+                  this.setState({
+                    minValue: min,
+                    maxValue: max
+                  });
+                }
+              }}
+            />
+          </View>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={{ color: style.COLOR_PRIMARY, fontSize: 30 }}>
+              {`${leftPad(deltaTime.hours)}`}
+            </Text>
+            <Text>h</Text>
+            <Text
+              style={{ color: style.COLOR_PRIMARY, fontSize: 30 }}
+            >{` ${leftPad(deltaTime.minutes)}`}</Text>
+            <Text>m</Text>
+          </View>
         </View>
       </View>
     );
