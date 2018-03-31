9da70087fce91ffddebd8b82fb3b043dac1a0926
Tony <tony@olm-mha.local>
2018-03-30 22:07:34 -0400

    adds time logging

--- src/reducers.js
+++ src/reducers.js
@@ -6,7 +6,16 @@ import authReducer from "./components/AuthScreen/reducers.js";
 const rootReducer = combineReducers({
   activitiesScreen: activitiesScreenReducer,
   calendarScreen: calendarScreenReducer,
-  auth: authReducer
+  auth: authReducer,
+  lastModified: (state = null, action) =>
+    [
+      "ACTIVITIES_SCREEN/NEW_ACTIVITY_COMPLETE",
+      "ACTIVITIES_SCREEN/ACTIVITY_DELETE",
+      "CALENDAR_SCREEN/NEW_ACTIVITY_LOG",
+      "CALENDAR_SCREEN/REMOVE_ACTIVITY_LOG"
+    ].some(type => action.type === type)
+      ? Date.now()
+      : state
 });
 
 export default rootReducer;
