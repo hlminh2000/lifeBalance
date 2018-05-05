2591f10bb979d2260fec5d3fa4d81a93714c038e
Tony <tony@olm-mha.local>
2018-05-04 22:25:54 -0400

    adds mutations

--- src/utils/api/queries.js
+++ src/utils/api/queries.js
@@ -24,3 +24,51 @@ export const allUserDataQuery = ({ idToken }) => `
     }
   }
 `;
+
+export const updateUserActivities = ({
+  idToken,
+  activitiesSet,
+  clientTimestamp
+}) => `
+{
+  mutation{
+    updateUserActivities(
+      idToken: ${idToken},
+      activityData: ${activitiesSet},
+      clientTimestamp: ${clientTimestamp}
+    ){
+      id
+      icon
+      title
+      createdAt
+      isActive
+      isArchived
+    }
+  }
+}
+`;
+
+export const updateUserActivityLogs = ({
+  idToken,
+  clientTimestamp,
+  activityLogs,
+  date
+}) => `
+{
+  mutation{
+    updateUserActivityLogs(
+      idToken: ${idToken},
+      clientTimestamp: ${clientTimestamp},
+      activityLogs: ${activityLogs},
+      date: ${date}
+    ){
+      id
+      activityId
+      timestamp
+      start
+      end
+      date
+    }
+  }
+}
+`;
