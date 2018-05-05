17334101096235294217d0fd2b07c0f2d17066ab
Tony <tony@olm-mha.local>
2018-05-05 00:35:03 -0400

    adds some hoc

--- src/utils/auth/index.js
+++ src/utils/auth/index.js
@@ -1,6 +1,27 @@
+import React from "react";
+import Component from "react-component-component";
 import facebookLogin from "./facebook";
 import googleLogin from "./google";
 import firebase from "react-native-firebase";
+
+export const UserContext = React.createContext(null);
+
+export const withIdToken = WrappedComponent => props => (
+  <UserContext.Consumer>
+    {user => (
+      <Component
+        initialState={{ idToken: null }}
+        didMount={({ setState }) => {
+          user.getIdToken().then(idToken => setState({ idToken }));
+        }}
+      >
+        {({ state: { idToken } }) => (
+          <WrappedComponent {...{ ...props, idToken }} />
+        )}
+      </Component>
+    )}
+  </UserContext.Consumer>
+);
 
 export default {
   facebookLogin,
