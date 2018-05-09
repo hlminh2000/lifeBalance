73d86933a92f12f70bf6fef0af6efeaece39f513
Tony <tony@olm-mha.local>
2018-05-08 23:53:14 -0400

    hards code prod api

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -2,9 +2,9 @@ import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import Component from "react-component-component";
 import { allUserDataQuery } from "./queries";
-const GRAPHQL_BASE = "http://192.168.100.108:3000/graphql";
-// const GRAPHQL_BASE =
-//   "https://us-central1-lifebalance-e467a.cloudfunctions.net/api/graphql";
+// const GRAPHQL_BASE = "http://192.168.100.108:3000/graphql";
+const GRAPHQL_BASE =
+  "https://us-central1-lifebalance-e467a.cloudfunctions.net/api/graphql";
 
 const alwaysSendHeaders = { "Content-Type": "application/json" };
 
